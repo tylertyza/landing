82690162f60c76857ff4d9ed2af2cb8be125ac23
Jakeoid <jake.triumphant@gmail.com>
2017-07-27 17:39:39 +1000

    Initial Site Commit

---
+++ js/ServerStatus.js
@@ -0,0 +1,23 @@
+function ServerStatus(serverip){
+	$.getJSON('http://mcapi.ca/query/' + serverip + '/extensive', function(json){
+		if (json.status !== true) {
+			$('#status').html("???");
+		} else {
+			$("#status").html(json.status ? 'online' : 'offline');	
+
+
+
+			var full_list = ""
+
+			if(json.list != null){
+				for(var i=0; i<json.list.length; ++i){
+					full_list = full_list + "<img src=\"https://crafatar.com/avatars/" + json.list[i] + "\" />"
+				}
+
+				$("#users-online").html(full_list);	
+			} else {
+				$("#users-online").html('<center>Nobody :(</center>')
+			}
+		}
+	});
+}
---
+++ js/status.js
@@ -0,0 +1,4 @@
+  
+// https://mcapi.ca/query/play.strongcraft.org/extensive
+
+ServerStatus('ilovebigdick.iceswag.com:36503');
