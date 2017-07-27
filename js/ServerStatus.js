78de3d8351b8b71c20f513578f5185582c3a721e
Jake <jake.triumphant@gmail.com>
2017-07-27 20:37:12 +1000

    Adding hat layers to avatars

--- js/ServerStatus.js
+++ js/ServerStatus.js
@@ -11,7 +11,7 @@ function ServerStatus(serverip){
 
 			if(json.list != null){
 				for(var i=0; i<json.list.length; ++i){
-					full_list = full_list + "<img src=\"https://crafatar.com/avatars/" + json.list[i] + "\" />"
+					full_list = full_list + "<img src=\"https://crafatar.com/avatars/" + json.list[i] + "\?overlay=true" />"
 				}
 
 				$("#users-online").html(full_list);	
