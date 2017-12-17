98fd1b484f7a43c7abbd0f1c7ebfe6a59be252e3
Tyler Conaln <tylertyza@users.noreply.github.com>
2017-12-18 05:45:29 +1100

    Update status.js

--- js/status.js
+++ js/status.js
@@ -1,4 +1,25 @@
-  
-// https://mcapi.ca/query/play.strongcraft.org/extensive
+function ServerStatus(serverip) {
+    $.getJSON('https://use.gameapis.net/mc/query/info/' + serverip, function (json) {
+
+        if (json.status !== true) {
+            $('#status').html("???");
+        } else {
+            $("#status").html(json.status ? 'online' : 'offline');
+
+            var full_list = ""
+
+            if (json.hover != null) {
+                for (var i = 0; i < json.hover.length; ++i) {
+                    full_list = full_list + "<img src=\"https://crafatar.com/renders/head/" + json.hover[i].name + "?overlay=True\" />"
+                }
+
+                $("#users-online").html(full_list);
+            } else {
+                $("#users-online").html('<center>Nobody :(</center>')
+            }
+        }
+
+    });
+}
 
 ServerStatus('ilovebigdick.iceswag.com:28685');
